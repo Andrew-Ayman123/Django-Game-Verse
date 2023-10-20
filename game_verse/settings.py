026a3cf1b223beccedf9154f891e227d13544d83
Andrew Ayman <andrewayman9@gmail.com>
2023-10-20 15:12:03 +0300

    Edited the settings

--- game_verse/settings.py
+++ game_verse/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a_hzcy252v*ku7nw#@1(b@ud79)*q#_1l(f9r_fhfjcqn-5(uw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["192.168.1.7"]
+ALLOWED_HOSTS = ["andrewayman.pythonanywhere.com"]
 
 
 # Application definition
