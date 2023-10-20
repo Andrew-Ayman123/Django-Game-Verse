7b1c4a1b6268454268aaf883e65970bd656dcbcb
Andrew Ayman <andrewayman9@gmail.com>
2023-10-20 20:23:29 +0300

    Added requirements

--- game_verse/asgi.py
+++ game_verse/asgi.py
@@ -9,16 +9,17 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
-from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter,URLRouter
+from django.core.asgi import get_asgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'game_verse.settings')
+application = get_asgi_application()
+
 from channels.auth import AuthMiddlewareStack
 import main.routing
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'game_verse.settings')
 
-# application = get_asgi_application()
 application = ProtocolTypeRouter(
   {
-    'http':get_asgi_application(),
+    'http':application,
     'websocket':AuthMiddlewareStack(
       URLRouter(
         main.routing.websocket_urlpatterns
